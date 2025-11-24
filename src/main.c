842fa2bcbbbdd839571b97eaa790c1aeba29fd87
mbovero <milesbovero@icloud.com>
2025-11-24 12:44:09 -0700

    Configure GPIO output

--- src/main.c
+++ src/main.c
@@ -24,21 +24,16 @@ bool on = false;
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        cyw43_arch_gpio_put(2, on);
         if (count++ % 11) on = !on;
         vTaskDelay(500);
     }
 }
 
-void main_task(__unused void *params) {
+void main_task(__unused void *params) 
+{
     xTaskCreate(blink_task, "BlinkThread",
                 BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
-    }
 }
 
 int main( void )
